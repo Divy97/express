881a52255ebcdab1a5e8d89079265d098f3263ef
Divy <divyparekh1810@gmail.com>
2022-12-18 13:09:09 +0530

    Done with rendering HTML

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 
+app.set("view engine", "ejs");
 app.get("/", (req, res) => {
   console.log("Here");
-  res.send("Hi");
+  //   res.send("Hi");
+  res.render("index", { text2332: "world" });
 });
 app.listen(3000);
