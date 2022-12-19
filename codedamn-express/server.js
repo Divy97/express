864b9b748fa4fc9d87afc1ecf9895a3ab27e73f8
Divy <divyparekh1810@gmail.com>
2022-12-19 18:24:03 +0530

    receiving data from front-end

--- codedamn-express/server.js
+++ codedamn-express/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
+const bodyParser = require("body-parser");
+
 // app.get("/", (re1, res) => [res.send("Hello World")]);
 // app.get("/something", (re1, res) => [res.send("Hello Something")]);
 // app.listen(8000, () => {
@@ -34,7 +36,16 @@ const app = express();
 
 // static data to server
 
-app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./public/index.html"));
+// app.get("/", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "./public/index.html"));
+// });
+
+// accept the data
+
+app.use("/", express.static(path.resolve(__dirname, "./public")));
+app.use(bodyParser.json());
+app.post("/data", (req, res) => {
+  console.log(req.body);
+  res.json({ status: "ok" });
 });
 app.listen(8000, console.log("listening on port 8000"));
