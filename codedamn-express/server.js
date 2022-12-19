bcc39c5f8bd5ffc8fab03368ec0b229defcc0106
Divy <divyparekh1810@gmail.com>
2022-12-19 18:01:02 +0530

    sending file to the browser

--- codedamn-express/server.js
+++ codedamn-express/server.js
@@ -1,6 +1,6 @@
-// const express = require("express");
-
-// const app = express();
+const express = require("express");
+const path = require("path");
+const app = express();
 
 // app.get("/", (re1, res) => [res.send("Hello World")]);
 // app.get("/something", (re1, res) => [res.send("Hello Something")]);
@@ -8,31 +8,33 @@
 //   console.log("server is running");
 // });
 
-const express = require("express");
-const app = express();
+// app.get("/", (req, res) => {
+//   res.send("hello world").end();
+// });
+
+// app.get("/what-is-my-method", (req, res) => {
+//   res.send("GET").end();
+// });
+
+// app.post("what-is-my-method", (req, res) => {
+//   res.send("POST").end();
+// });
+
+// app.put("what-is-my-method", (req, res) => {
+//   res.send("PUT").end();
+// });
+
+// app.delete("what-is-my-method", (req, res) => {
+//   res.send("DELETE").end();
+// });
+
+// app.patch("what-is-my-method", (req, res) => {
+//   res.send("PATCH").end();
+// });
+
+// static data to server
 
 app.get("/", (req, res) => {
-  res.send("hello world").end();
+  res.sendFile(path.resolve(__dirname, "./public/index.html"));
 });
-
-app.get("/what-is-my-method", (req, res) => {
-  res.send("GET").end();
-});
-
-app.post("what-is-my-method", (req, res) => {
-  res.send("POST").end();
-});
-
-app.put("what-is-my-method", (req, res) => {
-  res.send("PUT").end();
-});
-
-app.delete("what-is-my-method", (req, res) => {
-  res.send("DELETE").end();
-});
-
-app.patch("what-is-my-method", (req, res) => {
-  res.send("PATCH").end();
-});
-
-app.listen(8000, console.log("listening on port 1337"));
+app.listen(8000, console.log("listening on port 8000"));
