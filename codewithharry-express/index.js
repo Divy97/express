abf0c861f0e8150929718a209c103e142bb98dd6
Divy <divyparekh1810@gmail.com>
2022-12-20 12:00:11 +0530

    getting params

--- codewithharry-express/index.js
+++ codewithharry-express/index.js
@@ -3,17 +3,24 @@ const path = require("path");
 const app = express();
 const PORT = 8080;
 
-app.get("/", (req, res) => {
-  res.send("Hello world!!");
+// const divyMiddleWare = (req, res, next) => {
+//   console.log(req);
+// };
+
+// app.use(express.static(path.join(__dirname, "public")));
+// app.use(divyMiddleWare);
+
+app.get("/hello/:name", (req, res) => {
+  res.send("Hello " + req.params.name);
 });
 
-app.get("/about", (req, res) => {
-  // res.send("Hello about!!");
-  // res.sendFile(path.join(__dirname, "./index.html"));
-  res.json({
-    Name: "Divy",
-  });
-});
+// app.get("/about", (req, res) => {
+//   // res.send("Hello about!!");
+//   // res.sendFile(path.join(__dirname, "./index.html"));
+//   // res.json({
+//   //   Name: "Divy",
+//   // });
+// });
 
 app.listen(PORT, () => {
   console.log("Running");
